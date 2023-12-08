8db3860c890361fafbdb8911f93dd3a4fab2b027
Gorgees Odisho <gorgees041@gmail.com>
2023-12-08 08:03:18 +0300

    create a loading skeletong for cards details

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "./ui/img/avengers-logo.png";
 import CardsLoadingSkeltons from "./ui/components/loading-skeleton/CardsLoadingSkeletons";
+import DetailsSkeletonLoading from "./ui/components/loading-skeleton/DetailsSkeletonLoading";
 
 export default function Home() {
   return (
     <main className="text-white">
       <Link href={"/marvel"}>Marvel</Link>
+      <DetailsSkeletonLoading />
     </main>
   );
 }
---
+++ app/ui/components/loading-skeleton/DetailsSkeletonLoading.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const DetailsSkeletonLoading = () => {
+  return (
+    <div className="flex flex-col lg:flex-row mb-[50px] md:mb-[100px] text-white animate-pulse">
+      {/* Image Placeholder */}
+      <div className="w-40 h-60 lg:w-[200px] lg:h-[300px] m-5 border-4 border-gray-400  rounded bg-gray-700"></div>
+
+      <div className="m-5 lg:my-10 w-full">
+        {/* Title Placeholder */}
+        <div className="w-36 h-6 bg-gray-700 mb-4"></div>
+
+        {/* Description Placeholder */}
+        <div className="w-72 h-4 bg-gray-700 mb-2"></div>
+        <div className="w-80 h-4 bg-gray-700 mb-2"></div>
+        <div className="w-64 h-4 bg-gray-700 mb-2"></div>
+
+        {/* Date Placeholder */}
+        <div className="w-48 h-4 bg-gray-700 mt-10"></div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailsSkeletonLoading;

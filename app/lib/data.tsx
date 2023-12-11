f59d8dc0cea9f25fb2cb73bd7564371dd7e0da5f
Gorgees Odisho <gorgees041@gmail.com>
2023-12-11 22:18:21 +0300

    comment out the old fetchoing data

--- app/lib/data.tsx
+++ app/lib/data.tsx
@@ -279,7 +279,7 @@ export const fetchCategoryStories = async (
     characters: urlCharactersStories(id),
     comics: urlComicsStories(id),
     creators: urlCreatorsStories(id),
-    events: urlEventsSeries(id),
+    events: urlEventsStories(id),
     series: urlSeriesStories(id),
   };
   let URL;
@@ -392,146 +392,146 @@ export const fetchCategoryStories = async (
 ////////////////////////////////////////
 
 //////////fetch creators///////////
-export const fetchCreators = async (offset: string) => {
-  const URL = urlCreators(offset);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreators = async (offset: string) => {
+//   const URL = urlCreators(offset);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchCreatorsByName = async (name: string, offset: string) => {
-  const URL = urlCreatorsByName(name);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreatorsByName = async (name: string, offset: string) => {
+//   const URL = urlCreatorsByName(name);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchCreatorsById = async (id: string) => {
-  const URL = urlCreatorsById(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreatorsById = async (id: string) => {
+//   const URL = urlCreatorsById(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchCreatorsComics = async (id: string) => {
-  const URL = urlCreatorsComics(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreatorsComics = async (id: string) => {
+//   const URL = urlCreatorsComics(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchCreatorsEvents = async (id: string) => {
-  const URL = urlCreatorsEvents(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreatorsEvents = async (id: string) => {
+//   const URL = urlCreatorsEvents(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchCreatorsSeries = async (id: string) => {
-  const URL = urlCreatorsSeries(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreatorsSeries = async (id: string) => {
+//   const URL = urlCreatorsSeries(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchCreatorsStories = async (id: string) => {
-  const URL = urlCreatorsStories(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchCreatorsStories = async (id: string) => {
+//   const URL = urlCreatorsStories(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
 ////////////////////////////////////////
 
 //////////fetch Series///////////
-export const fetchSeries = async (offset: string) => {
-  const URL = urlSeries(offset);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeries = async (offset: string) => {
+//   const URL = urlSeries(offset);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesByTitle = async (title: string, offset: string) => {
-  const URL = urlSeriesByTitle(title, offset);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesByTitle = async (title: string, offset: string) => {
+//   const URL = urlSeriesByTitle(title, offset);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesById = async (id: string) => {
-  const URL = urlSeriesById(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesById = async (id: string) => {
+//   const URL = urlSeriesById(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesCharacters = async (id: string) => {
-  const URL = urlSeriesCharacters(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesCharacters = async (id: string) => {
+//   const URL = urlSeriesCharacters(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesComics = async (id: string) => {
-  const URL = urlSeriesComics(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesComics = async (id: string) => {
+//   const URL = urlSeriesComics(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesCreators = async (id: string) => {
-  const URL = urlSeriesCreators(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesCreators = async (id: string) => {
+//   const URL = urlSeriesCreators(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesEvents = async (id: string) => {
-  const URL = urlSeriesEvents(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesEvents = async (id: string) => {
+//   const URL = urlSeriesEvents(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchSeriesStories = async (id: string) => {
-  const URL = urlSeriesStories(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchSeriesStories = async (id: string) => {
+//   const URL = urlSeriesStories(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
 ////////////////////////////////////////
 
 /////////fetch Events//////////
-export const fetchEvents = async (offset: string) => {
-  const URL = urlEvents(offset);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchEvents = async (offset: string) => {
+//   const URL = urlEvents(offset);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchEventsByTitle = async (title: string, offset: string) => {
-  const URL = urlEventsByTitle(title, offset);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchEventsByTitle = async (title: string, offset: string) => {
+//   const URL = urlEventsByTitle(title, offset);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchEventsById = async (id: string) => {
-  const URL = urlEventsById(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchEventsById = async (id: string) => {
+//   const URL = urlEventsById(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchEventsCharacters = async (id: string) => {
-  const URL = urlEventsCharacters(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchEventsCharacters = async (id: string) => {
+//   const URL = urlEventsCharacters(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchEventsComics = async (id: string) => {
-  const URL = urlEventsComics(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchEventsComics = async (id: string) => {
+//   const URL = urlEventsComics(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
-export const fetchEventsCreators = async (id: string) => {
-  const URL = urlEventsCreators(id);
-  const res = await fetch(URL);
-  return res.json();
-};
-export const fetchEventsSeries = async (id: string) => {
-  const URL = urlEventsSeries(id);
-  const res = await fetch(URL);
-  return res.json();
-};
-export const fetchEventsStories = async (id: string) => {
-  const URL = urlEventsStories(id);
-  const res = await fetch(URL);
-  return res.json();
-};
+// export const fetchEventsCreators = async (id: string) => {
+//   const URL = urlEventsCreators(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
+// export const fetchEventsSeries = async (id: string) => {
+//   const URL = urlEventsSeries(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
+// export const fetchEventsStories = async (id: string) => {
+//   const URL = urlEventsStories(id);
+//   const res = await fetch(URL);
+//   return res.json();
+// };
 
 ////////////////////////////////////////

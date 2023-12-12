eb32b24033a3ee9bf63a90c442e1692d5b85055c
Gorgees Odisho <gorgees041@gmail.com>
2023-12-12 08:32:22 +0300

    add avengers image to home page #14

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import HomeSearchBar from "./ui/components/home/HomeSearchBar";
-import HomeCategories from "./ui/components/home/HomeCategories";
-import HomeSearchContents from "./ui/components/home/HomeSearchContents";
+import avengersImg from "@/app/ui/img/avengers-1.png";
 
 export default function Home({
   searchParams,
@@ -12,28 +10,25 @@ export default function Home({
   const query = searchParams.query;
   const category = searchParams.category;
   return (
-    <main className="text-white flex items-center flex-col mt-[80px] mx-5">
+    <main className="text-white flex items-center flex-col my-[80px] mx-[50px] sm:my-[120px] sm:mx-[180px]">
       <div className="text-center mt-10">
         <h1 className="text-3xl sm:text-6xl mb-2 font-bold">
           Welcome to G-Marvel website
         </h1>
-        <p className="text-slate-300">
+        <p className="text-slate-300 my-10">
           Here we only talk about our marvel universe & our heros
         </p>
       </div>
       <div>
+        <Image src={avengersImg} alt="avengers image" width={500} height={0} />
+      </div>
+      <div>
         <Link href={"/marvel"}>
-          <h2 className="mt-5 text-lg hover:text-marvelRed">
-            Fine out more &gt;&gt;
+          <h2 className="mt-5 text-lg text-center border-4 bg-marvelRed text-white p-2 hover:bg-offWhite hover:border-marvelRed hover:text-marvelRed">
+            Explore the Marvel Universe &gt;&gt;
           </h2>
         </Link>
       </div>
-
-      {/* <div className="text-center">
-        <HomeSearchBar />
-        <HomeCategories />
-        <HomeSearchContents query={query} category={category} />
-      </div> */}
     </main>
   );
 }
